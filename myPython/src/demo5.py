87d8c1fabf24662c6cdbcae2a7032f5f83f2b259
liuchao-github <liuchao_mail@qq.com>
2017-07-06 00:07:20 +0800

    Signed-off-by: liuchao-github <liuchao_mail@qq.com>

--- myPython/src/demo5.py
+++ myPython/src/demo5.py
@@ -1,1 +1,62 @@
 # coding=utf-8
+"""
+Python Number
+"""
+"""
+数据类型是不允许改变的，这就意味着如果改变Number数据类型的值，
+将重新分配内存空间。
+以下实例在赋值时Number被创建
+"""
+var1 = 1
+var2 = 10
+var3 = 100
+
+del var1
+del var2, var3
+
+"""
+Python支持四种数值类型：
+int
+long integers
+floating point real values
+complex numbers
+"""
+"""
+Python Number类型转换
+"""
+x = 5
+L = long(x)
+f = float(L)
+print L
+print f
+
+"""
+Python数学函数：
+abs() fabs()
+ceil() floor() round()
+cmp() 
+max() min()
+exp() pow()
+log() log10()
+modf()
+sqrt() 
+"""
+"""
+Python随机函数
+choice()
+randrange()
+random()
+seed()
+shuffle()
+uniform()
+"""
+
+"""
+Python三角函数
+sin() tan() cos()
+degrees() radians()
+"""
+
+"""
+Python数学常量
+"""
--- myPython/src/demo6.py
+++ myPython/src/demo6.py
@@ -1,1 +1,64 @@
 # coding=utf-8
+"""
+Python字符串
+"""
+"""
+访问字符串中的值
+"""
+var1 = 'HelloWorld'
+print var1[0], var1[1:5]
+
+"""
+字符串更新
+可以对已经存在的字符串进行修改，并赋值给另一个变量
+"""
+str1 = "Hello World"
+print str1[:6]+"baby"
+
+"""
+转义字符
+\ \\ \' \" 
+\a \b \e \000 
+\n \v \t \r \f
+# \oyy  \other
+"""
+
+"""
+字符串运算符
++ * 
+[] [:]
+in      not in
+r/R %
+"""
+
+"""
+字符串格式化
+最基本的用法是将一个值插入到一个有字符串格式符%s的字符串中
+"""
+print "My name is %s and weight is %d kg" % ("lc", 85)
+
+"""
+三引号（triple quotes）
+三引号可以将复杂的字符串进行赋值：
+允许一个字符串跨多行，字符串中可以包含换行符、制表符以及其他特殊字符
+保持WYSIWYG
+"""
+"""
+Unicode字符串
+"""
+print u"Hello\u0020World"
+
+"""
+字符串内建函数
+"""
+string = "Hello World"
+n = string.count("o")
+print n
+string.endswith("d")
+string.find("H")
+string.format()
+string.index()
+string.join()
+string.replace()
+string.split()
+string.translate()
