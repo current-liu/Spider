18ef8cb2daca9088b0e11e8c2aab52b92d0e9a58
liuchao-github <liuchao_mail@qq.com>
2017-07-26 00:43:18 +0800

    night work

---
+++ myPython1/demo/demo01.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+
+import random
+import base64
+from Proxies import Proxies
+
+
+class RandomUserAgent(object):
+    """Randomly rotate user agents based on a list of predefined ones"""
+
+    def __init__(self, agents):
+        self.agents = agents
+
+    def from_crawler(cls, crawler):
+        return cls(crawler.settings.getlist('USER_AGENTS'))
+
+    def process_request(self, requset, spider):
+        # print "*********" + random.choice(self.agents)
+        requset.headers.setdefault('User-Agent', random.choice(self.agents))
+
+
+class ProxyMiddleware(object):
+    def process_requset(self,request,spider):
+        proxy = random.choice(PROXIES)
+        if proxy['user_pass'] is not None:
+            request.meta['proxy'] = "http://%s" % proxy['ip_port']
+            encoded_user_pass = base64.encodestring(proxy['user_pass'])
+            request.headers['Proxy-Authorization'] = "Basic" + encoded_user_pass
+            print "**************ProxyMiddleware have pass************" + proxy['ip_port']
+        else:
+            print "**************ProxyMiddleware no pass************" + proxy['ip_port']
+            request.meta['proxy'] = "http://%s" % proxy['ip_port']
